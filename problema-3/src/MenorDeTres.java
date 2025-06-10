71a1e58802b0c053a798baf877f53385a0e0cc2e
Rafael Augusto Balko de Souza <rafaelbalko@gmail.com>
2025-06-10 20:51:34 -0300

    Resolução do problema 3 !

--- problema-3/src/MenorDeTres.java
+++ problema-3/src/MenorDeTres.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MenorDeTres {
     public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite o primeiro valor: ");
+        int a = sc.nextInt();
+
+        System.out.print("Digite o segundo valor:");
+        int b = sc.nextInt();
+
+        System.out.print("Digite o terceiro valor:");
+        int c = sc.nextInt();
+
+        int menor = a;
+
+        if (b < menor){
+            menor = b;
+        }
+        if (c < menor) {
+            menor = c;
+        }
+        System.out.println("MENOR = " + menor);
+
+        sc.close();
     }
 }
